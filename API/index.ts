40041d758d35d8ed6223a89882dd4a1088c1e034
Bektur Batyrbekov <batyrbekovbektur0@gmail.com>
2024-04-16 15:42:53 +0600

    added homework-82

---
+++ API/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import musicRoute from "./routes/music";
+import mongoose from "mongoose";
+import config from "./config";
+
+const app = express();
+const port= 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/', musicRoute);
+
+const run = async () => {
+   await mongoose.connect(config.mongoose.db);
+
+   app.listen(port, () => {
+      console.log(`port ${port}`);
+   });
+
+   process.on('exit', () => {
+      mongoose.disconnect()
+   });
+}
+
+void run();
+
